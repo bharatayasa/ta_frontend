2165bfd296217465c3b148ca7fe223a156076a6f
Wayan_Bharata <bharatayasa40@gmail.com>
2023-10-29 23:12:42 +0800

    edit user done

--- src/component/usercomponent/EditUser.jsx
+++ src/component/usercomponent/EditUser.jsx
@@ -4,20 +4,20 @@ import { useNavigate } from "react-router-dom";
 import EditButton from "./EditButton";
 
 export default function EditUser({ user }) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  async function onEditUserHandler(user) {
-    console.log(user);
-    const response = await updateuser(user);
-    console.log(response);
+	async function onEditUserHandler(user) {
+		console.log(user);
+		const response = await updateuser(user);
+		console.log(response);
 
-    if (!response.error) {
-      navigate("/");
-      window.location.reload();
-    } else {
-      console.log("Update user failed:", response.error);
-    }
-  }
+		if (!response.error) {
+		navigate("/");
+		window.location.reload();
+		} else {
+		console.log("Update user failed:", response.error);
+		}
+	}
 
-  return <EditButton {...user} updateuser={onEditUserHandler} />;
+	return <EditButton {...user} updateuser={onEditUserHandler} />;
 }
