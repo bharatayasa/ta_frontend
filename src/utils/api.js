82643d4646af8601b3d7084a73ee7df7384f0818
Abel Yoshuara <abelyoshuara@gmail.com>
2023-11-22 19:01:19 +0800

    add config env

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:5000";
-const BASE_URL = "https://backend-405811.et.r.appspot.com";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 function getAccessToken() {
     return localStorage.getItem("accessToken");
